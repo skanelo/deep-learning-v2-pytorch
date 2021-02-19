2ea3ec75f222c28f993da8ab9055a6c0a2b720db
skanelo <stefanos.kanellopoulos@yahoo.com>
2021-02-19 18:38:54 +0000

    My first ensemble of files

--- intro-to-pytorch/main.py
+++ intro-to-pytorch/main.py
@@ -1,6 +1,10 @@
 import torch
+from rand_init import initialize
+from activation import activation_func
+
+features, weights, bias = initialize()
 
 ## Calculate the output of this network using the weights and bias tensors
-k = activation(torch.matmul(features, weights.view(5, 1)).add(bias))
+k = activation_func(torch.matmul(features, weights.view(5, 1)).add(bias))
 
 print(k)
---
+++ intro-to-pytorch/rand_init.py
@@ -0,0 +1,14 @@
+import torch
+
+def initialize():
+    ### Generate some data
+    torch.manual_seed(7) # Set the random seed so things are predictable
+
+    # Features are 5 random normal variables
+    features = torch.randn((1, 5))
+    # True weights for our data, random normal variables again
+    weights = torch.randn_like(features)
+    # and a true bias term
+    bias = torch.randn((1, 1))
+
+    return features, weights, bias
